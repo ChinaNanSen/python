b0e779a69b50892863a04c17044c4e2fad9ddb57
ChinaNanSen <839726302@qq.com>
2022-11-08 19:06:25 +0800

    update

--- zuoye.py
+++ zuoye.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python
 import sys
+#加个注释
 username = 'zzn'
 password = '123456'
 locked = 1
 retry_counter = 0
 while retry_counter < 3:
-     user = raw_input('Username:').strip()
+     user = input('Username:').strip()
      if len(user) == 0:
-         print '\033[31;1mUsername cannot be empty!\033[0m'
+         print ('\033[31;1mUsername cannot be empty!\033[0m')
          #continue
          #break
-     passwd = raw_input('Password:').strip()
+     passwd = input('Password:').strip()
      if len(passwd) == 0:
-	     print '\033[031;1mPassword cannot be empty!\033[0m'
+	     print ('\033[031;1mPassword cannot be empty!\033[0m')
          #continue
          #break
  #handle the username and passwd empty issue
  #going to the loging verification part
  #means the user is locked
      if locked == 0:
-         print 'Your username is locked!'
+         print ('Your username is locked!')
          sys.exit() 
      else:
          if user == username and passwd == password:
@@ -27,4 +28,4 @@ while retry_counter < 3:
              sys.exit('Welcome %s login to our system!' % user)
          else:
              retry_counter += 1
-             print '\033[31;1mWrong username or password,you have %s more chances!\033[0m' % (3 - retry_counter)
+             print ('\033[31;1mWrong username or password,you have %s more chances!\033[0m' % (3 - retry_counter))
