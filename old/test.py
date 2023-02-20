6b94c25ad575f14682e3dd20e9624f6e0e98ceda
zhangzhinan <zhinan.zhang@vevor.net>
2023-02-20 14:15:26 +0800

    update

--- old/test.py
+++ old/test.py
@@ -1,8 +1,27 @@
-a = 1.8
-print(a)
-print(a,type(a))
-print(10 // 3)
-print(round(10 / 3,2))
-print(10 % 3)
-	
+# a = 1.8
+# print(a)
+# print(a,type(a))
+# print(10 // 3)
+# print(round(10 / 3,2))
+# print(10 % 3)
 
+a = lambda x, y: x + y
+print(a(3, 4))
+
+
+def func(x, y):
+    return x + y
+
+
+print(func(5, 6))
+
+
+def test(f, a, b):
+    print("test")
+    print(f(a, b))
+
+
+test(func, 3, 5)
+
+re = map((lambda x,y: x+y),[1,2,3],[6,7,9])
+print(re)
