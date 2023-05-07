737122bf343609530d71c79e9ae3a806ffcc8779
zhangzhinan <zhinan.zhang@vevor.net>
2023-05-07 18:41:19 +0800

    fix

--- jy/okex-btc.py
+++ jy/okex-btc.py
@@ -3,18 +3,18 @@ import time
 import ccxt
 import numpy as np
 import config
+import logging
+from decimal import Decimal
+
+# 设置日志记录
+logging.basicConfig(filename='trading_bot.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def fetch_ohlcv():
     ohlcv = exchange.fetch_ohlcv(symbol, timeframe)
     return np.array(ohlcv)
 
-
 def calculate_moving_average(data, length):
     return np.convolve(data[:, 4], np.ones(length) / length, mode='valid')
-
-
-#def calculate_moving_average(data, length):
-    #return data[-length:, 4].mean()
 
 def log_trade_to_file(file_name, trade_info):
     if not os.path.exists(file_name):
@@ -38,6 +38,7 @@ slow_ma_length = 20
 min_trade_amount = 0.0001
 
 last_operation = None
+last_buy_price = None
 
 while True:
     try:
@@ -50,55 +51,51 @@ while True:
         balance = exchange.fetch_balance()
         eth_balance = balance.get('ETH', {}).get('free', 0)
         usdt_balance = balance.get('USDT', {}).get('free', 0)
-        print(eth_balance)
-        print(usdt_balance)
-    
 
         if last_operation != 'buy' and fast_ma[-1] > slow_ma[-1] and usdt_balance > 1:
-            amount = usdt_balance / data[-1, 4]
+            amount = Decimal(usdt_balance) / Decimal(data[-1, 4])
             if amount >= min_trade_amount:
                 order = exchange.create_market_buy_order(symbol, amount)
-                print("order = ", order)
 
                 last_operation = 'buy'
-                print('已买入 ETH')
+                last_buy_price = Decimal(order['info']['fillPx'])
 
                 fee_currency = order['info']['feeCcy']
-                fee_amount = float(order['info']['fee'])
+                fee_amount = Decimal(order['info']['fee'])
                 if fee_currency != 'USDT':
                     fee_conversion_symbol = f"{fee_currency}/USDT"
                     fee_conversion_rate = exchange.fetch_ticker(fee_conversion_symbol)['close']
-                    fee_in_usdt = fee_amount * fee_conversion_rate
+                    fee_in_usdt = fee_amount * Decimal(fee_conversion_rate)
                 else:
                     fee_in_usdt = fee_amount
 
                 trade_info = f"{order['info']['ts']},Buy,{order['info']['fillPx']},{order['info']['fillSz']},N/A,{fee_in_usdt}"
-                print(trade_info)
+                logging.info(trade_info)
                 log_trade_to_file('./trades.csv', trade_info)
 
         elif last_operation != 'sell' and fast_ma[-1] < slow_ma[-1] and eth_balance * data[-1, 4] > 1:
             if eth_balance >= min_trade_amount:
                 order = exchange.create_market_sell_order(symbol, eth_balance)
-                print("sell order =  ",order)
 
                 last_operation = 'sell'
-                print('已售出 ETH')
 
                 fee_currency = order['info']['feeCcy']
-                fee_amount = float(order['info']['fee'])
+                fee_amount = Decimal(order['info']['fee'])
                 if fee_currency != 'USDT':
                     fee_conversion_symbol = f"{fee_currency}/USDT"
                     fee_conversion_rate = exchange.fetch_ticker(fee_conversion_symbol)['close']
-                    fee_in_usdt = fee_amount * fee_conversion_rate
+                    fee_in_usdt = fee_amount * Decimal(fee_conversion_rate)
                 else:
                     fee_in_usdt = fee_amount
+                sell_price = Decimal(order['info']['fillPx'])
+            profit_loss_percentage = (sell_price - last_buy_price) / last_buy_price * 100
 
-                trade_info = f"{order['info']['ts']},Sell,{order['info']['fillPx']},{order['info']['fillSz']},N/A,{fee_in_usdt}"
-                print(trade_info)
-                log_trade_to_file('./trades.csv', trade_info)
+            trade_info = f"{order['info']['ts']},Sell,{order['info']['fillPx']},{order['info']['fillSz']},{profit_loss_percentage},{fee_in_usdt}"
+            logging.info(trade_info)
+            log_trade_to_file('./trades.csv', trade_info)
 
         time.sleep(60 * 15)
     except Exception as e:
-        print(e)
+        logging.error(e)
         time.sleep(60)
 
