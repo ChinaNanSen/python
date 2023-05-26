4848483b22a8f0a8d6657d2f2423cd8792322485
zhangzhinan <zhinan.zhang@vevor.net>
2023-05-26 16:40:21 +0800

    fix

--- jy/getbtc-data.py
+++ jy/getbtc-data.py
@@ -1,19 +1,49 @@
+import os
+import time
 import ccxt
+import numpy as np
 import config
 
-symbol = 'BTC/USDT'
+exchange = ccxt.okex({
+    'apiKey': config.api_key,
+    'secret': config.secret_key,
+    'password': config.password,
+    'enableRateLimit': True,
+})
 
-def get_live_data():
-    exchange = ccxt.okex({
-        'apiKey': config.api_key,
-        'secret': config.secret_key,
-        'password': config.password,
-        'enableRateLimit': True,
-    })
+def log_trade_to_file(file_name, trade_info):
+    if not os.path.exists(file_name):
+        with open(file_name, 'w') as f:
+            f.write('Timestamp,Operation,Price,Amount,Fee,Fee in USDT\n')
+            print(f"Created file: {file_name}")
 
-    ticker = exchange.fetch_ticker(symbol)
-    return ticker
+    with open(file_name, 'a') as f:
+        f.write(trade_info + '\n')
+        print(f"Logged trade: {trade_info}")
 
-if __name__ == "__main__":
-    live_data = get_live_data()
-    print(live_data)
+def fetch_order_fee(order_id, symbol):
+    order_info = exchange.fetch_order(order_id, symbol)
+    print(f"Order info: {order_info}")  # 添加此行以查看'order_info'内容
+    fee_currency = order_info['info']['feeCcy']  # 修改此行
+    fee_amount = abs(float(order_info['info']['fee']))  # 修改此行
+    return fee_currency, fee_amount
+
+
+# order_id = '575409374068699136'  # 请替换为您的实际订单ID
+# symbol = 'ETH/USDT'
+
+# order = fetch_order_fee(order_id, symbol)
+# # print(order)
+# # print(order['info']['ordId'])
+# trade_info = "%s,%s"%(order[0],order[1])
+# log_trade_to_file('.\\test.csv',trade_info)
+
+file_path = 'file.json'  # 文件路径
+file_mode = 'w'  # 打开文件的模式，'w'表示写入模式
+
+with open(file_path, file_mode) as file:
+    # 写入内容
+    file.write('Hello, world!\n')
+    file.write('This is a test file.\n')
+    file.write('Writing some text.\n')
+
--- jy/okex_btc.py
+++ jy/okex_btc.py
@@ -3,11 +3,19 @@ import time
 import ccxt
 import numpy as np
 import config
-import logging
-from decimal import Decimal
+import math
 
-# 设置日志记录
-logging.basicConfig(filename='trading_bot.log', level=logging.INFO, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+def calculate_performance_metrics(file_name):
+    trades = np.genfromtxt(file_name, delimiter=',', skip_header=1)
+    if trades.shape[0] < 2:
+        return 0, 0, 0
+    profits = trades[:, 4] * trades[:, 2]
+    total_profit = np.sum(profits)
+    profit_factor = np.sum(profits[profits > 0]) / np.abs(np.sum(profits[profits < 0]))
+    daily_returns = (trades[1:, 4] - trades[:-1, 4]) / trades[:-1, 4]
+    sharpe_ratio = (np.mean(daily_returns) * 252) / (np.std(daily_returns) * math.sqrt(252))
+    return total_profit, profit_factor, sharpe_ratio
+
 
 def fetch_ohlcv():
     ohlcv = exchange.fetch_ohlcv(symbol, timeframe)
@@ -16,13 +24,23 @@ def fetch_ohlcv():
 def calculate_moving_average(data, length):
     return np.convolve(data[:, 4], np.ones(length) / length, mode='valid')
 
+def fetch_order_fee(order_id, symbol):
+    order_info = exchange.fetch_order(order_id, symbol)
+    fee_currency = order_info['info']['feeCcy']
+    fee_amount = abs(float(order_info['info']['fee']))
+    return fee_currency, fee_amount
+
 def log_trade_to_file(file_name, trade_info):
     if not os.path.exists(file_name):
         with open(file_name, 'w') as f:
-            f.write('Timestamp,Operation,Price,Amount,Profit/Loss Percentage,Fee in USDT\n')
+            f.write('Timestamp,Operation,Price,Amount,Fee,Fee in USDT,Total Profit,Profit Factor,Sharpe Ratio\n')
+            print(f"Created file: {file_name}")
 
     with open(file_name, 'a') as f:
+        total_profit, profit_factor, sharpe_ratio = calculate_performance_metrics(file_name)
+        trade_info += f',{total_profit},{profit_factor},{sharpe_ratio}'
         f.write(trade_info + '\n')
+        print(f"Logged trade: {trade_info}")
 
 exchange = ccxt.okex({
     'apiKey': config.api_key,
@@ -32,13 +50,12 @@ exchange = ccxt.okex({
 })
 
 symbol = 'ETH/USDT'
-timeframe = '15m'
+timeframe = '1m'
 fast_ma_length = 5
 slow_ma_length = 20
 min_trade_amount = 0.0001
 
 last_operation = None
-last_buy_price = None
 
 while True:
     try:
@@ -53,49 +70,53 @@ while True:
         usdt_balance = balance.get('USDT', {}).get('free', 0)
 
         if last_operation != 'buy' and fast_ma[-1] > slow_ma[-1] and usdt_balance > 1:
-            amount = Decimal(usdt_balance) / Decimal(data[-1, 4])
+            print("buy")
+            amount = usdt_balance / data[-1, 4]
             if amount >= min_trade_amount:
                 order = exchange.create_market_buy_order(symbol, amount)
+                order_id = order['id']
+                fee_currency, fee_amount = fetch_order_fee(order_id, symbol)
 
                 last_operation = 'buy'
-                last_buy_price = Decimal(order['info']['fillPx'])
 
                 fee_currency = order['info']['feeCcy']
-                fee_amount = Decimal(order['info']['fee'])
+                fee_amount = float(order['info']['fee'])
                 if fee_currency != 'USDT':
                     fee_conversion_symbol = f"{fee_currency}/USDT"
                     fee_conversion_rate = exchange.fetch_ticker(fee_conversion_symbol)['close']
-                    fee_in_usdt = fee_amount * Decimal(fee_conversion_rate)
+                    fee_in_usdt = fee_amount * fee_conversion_rate
                 else:
                     fee_in_usdt = fee_amount
 
-                trade_info = f"{order['info']['ts']},Buy,{order['info']['fillPx']},{order['info']['fillSz']},N/A,{fee_in_usdt}"
-                logging.info(trade_info)
-                log_trade_to_file('./trades.csv', trade_info)
+                trade_info = {'Timestamp': order['datetime'], 'Operation': 'Buy', 'Price': order['info']['fillPx'], 'Amount': order['amount'], 'Fee': fee_in_usdt}
+                print(trade_info)
+                log_trade_to_file('trades.json', trade_info)
 
         elif last_operation != 'sell' and fast_ma[-1] < slow_ma[-1] and eth_balance * data[-1, 4] > 1:
+            print("sell")
             if eth_balance >= min_trade_amount:
                 order = exchange.create_market_sell_order(symbol, eth_balance)
+                order_id = order['id']
+                fee_currency, fee_amount = fetch_order_fee(order_id,symbol)
 
                 last_operation = 'sell'
 
                 fee_currency = order['info']['feeCcy']
-                fee_amount = Decimal(order['info']['fee'])
+                fee_amount = float(order['info']['fee'])
                 if fee_currency != 'USDT':
                     fee_conversion_symbol = f"{fee_currency}/USDT"
                     fee_conversion_rate = exchange.fetch_ticker(fee_conversion_symbol)['close']
-                    fee_in_usdt = fee_amount * Decimal(fee_conversion_rate)
+                    fee_in_usdt = fee_amount * fee_conversion_rate
                 else:
                     fee_in_usdt = fee_amount
-                sell_price = Decimal(order['info']['fillPx'])
-            profit_loss_percentage = (sell_price - last_buy_price) / last_buy_price * 100
 
-            trade_info = f"{order['info']['ts']},Sell,{order['info']['fillPx']},{order['info']['fillSz']},{profit_loss_percentage},{fee_in_usdt}"
-            logging.info(trade_info)
-            log_trade_to_file('./trades.csv', trade_info)
+                trade_info = {'Timestamp': order['datetime'], 'Operation': 'Sell', 'Price': order['info']['fillPx'], 'Amount': order['amount'], 'Fee': fee_in_usdt}
+                print(trade_info)
+                log_trade_to_file('trades.json', trade_info)
 
-        time.sleep(60 * 15)
-    except Exception as e:
-        logging.error(e)
         time.sleep(60)
+    except Exception as e:
+        print(e)
+        time.sleep(60)
+
 
