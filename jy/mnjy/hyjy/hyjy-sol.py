120391a047ba32c21ecd0e658f851f012f94afed
zhangzhinan <zhinan.zhang@vevor.net>
2023-11-24 15:50:38 +0800

    fix

--- jy/mnjy/hyjy/hyjy-sol.py
+++ jy/mnjy/hyjy/hyjy-sol.py
@@ -73,6 +73,9 @@ def trading_logic(data_frame, position_opened):
     bl = bbands.iloc[-1]['BB_LOWER']
     cn = data_frame['close'].iloc[0]
     hn = data_frame['high'].iloc[0]
+    print("cn:%s\nbl:%s\n" %(cn, bl))
+    print("-------------")
+    print("cn:%s\nbu:%s\n" %(cn, bu))
 
     if float(cn) < bl and  position_opened == False:
         return "buy"
@@ -91,21 +94,13 @@ def execute_trade(trade_type, order_id):
         # 买入逻辑
         order_id = generate_order_id()
         print("-----")
-        
         # 买入信号
-        
         ye = account_balance("USDT")
-
         ccb = ye["details"][0]["availBal"]
         cb = float(ccb) / 2
-       
         print(position_opened)
-        # exit(1023)
-        
-
         # exit(1036)
         if float(cb) >= 100:
-
             result = tradeAPI.place_order(
                 instId=bz,
                 tdMode="cross",  # 保证金模式：isolated：逐仓 ；cross：全仓
@@ -140,9 +135,6 @@ def execute_trade(trade_type, order_id):
             oidict['bxf'] = bxf
             oidict['bcj'] = bcj
             oidict['bsx'] = bsx
-           
-            
-
             print("\033[32m++++hit++buy\033[0m")
         else:
             print("\033[31mbuy操作忽略,USDT余额不足\033[0m")
@@ -156,10 +148,7 @@ def execute_trade(trade_type, order_id):
             byex = getOrder("buy"+str(order_id))['data'][0]['fillSz']
         except Exception as es:
             print(f"\033[31m没有买入订单,忽略: {es} {byex}\033[0m")
-            
-
         if float(byex) != 0:
-
             uresult = tradeAPI.close_positions(
                 instId=bz,
                 # ccy='BTC',
@@ -168,7 +157,6 @@ def execute_trade(trade_type, order_id):
                 mgnMode="cross"
             )
             print(uresult)
-
             position_opened = False
             uoid = uresult['data'][0]['ordId']
             print(uoid)
@@ -185,10 +173,7 @@ def execute_trade(trade_type, order_id):
             oidict['ubxf'] = uxf
             oidict['ubcj'] = ucj
             oidict['ubsx'] = usx
-            
-
             print("\033[32m---hit-----sell\033[0m")
-
         else:
             print("\033[31msell操作忽略,BTC余额不足\033[0m")
     else:
@@ -196,7 +181,7 @@ def execute_trade(trade_type, order_id):
 
 def main():
     position_opened = False
-   
+    print("\033[34m~~~~~starting jy %s\033[0m" % dbz)
     while True:
         time.sleep(3)
         data_frame = get_historical_data()
@@ -207,6 +192,7 @@ def main():
         if trade_type in ["buy", "sell"]:
             execute_trade(trade_type, order_id)
             position_opened = (trade_type == "buy")
+        
 
         print("当前持仓状态:", position_opened)
 
